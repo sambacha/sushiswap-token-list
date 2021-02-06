e54f553afd8856065bf4ca52abbc629373660109
sam <sam@freighttrust.com>
2021-02-06 11:52:42 -0800

    feat(release): sushiswap

---
+++ index.mjs
@@ -0,0 +1,11 @@
+import defaultExport from "@uniswap/token-lists";
+import Ajv from "ajv";
+import * as data from "./tokens.json";
+
+var ajv = new Ajv();
+var valid = ajv
+  .addSchema(defaultExport.schema, "SushiSwap")
+  .validate("SushiSwap", data.default);
+if (!valid) {
+  throw new Error(ajv.errorsText());
+}
