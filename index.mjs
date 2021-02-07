4e09bc34400e9c566be9e430367cdce583a985fb
sam <sam@freighttrust.com>
2021-02-07 03:13:56 -0800

    fix(ajv): ajv constructor fix

--- index.mjs
+++ index.mjs
@@ -1,7 +1,10 @@
-'use strict';
+import defaultExport from '@uniswap/token-lists';
+import Ajv from 'ajv';
+import * as data from './sushiswap.tokens.json';
+import addFormats from 'ajv-formats';
 
-const schema = require('./schema.tokenlist.json');
-const validate = require('./validate');
-
-module.exports.schema = schema;
-module.exports.validate = validate;
+var ajv = new Ajv();
+var valid = ajv.addSchema(defaultExport.schema, 'mySchema').validate('mySchema', data.default);
+if (!valid) {
+  throw new Error(ajv.errorsText());
+}
